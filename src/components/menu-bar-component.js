f300f62b02b5338bcf58a6b7b46357a1f85bad6a
Bob marinko <robertpaulmarinko@yahoo.com>
2021-04-21 06:19:18 -0400

    Got menu to open and close in mobile view

--- src/components/menu-bar-component.js
+++ src/components/menu-bar-component.js
@@ -1,19 +1,18 @@
 const template = document.createElement('template');
 template.innerHTML = `
 <link rel="stylesheet" href="/styles/bulma.min.css">
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar is-info" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
         <a class="navbar-item" href="/picture-of-the-day">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+            <img src="/images/nav-page-icon.png">
         </a>
-
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
         </a>
     </div>
-    <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-menu">
         <div class="navbar-start">
             <a class="navbar-item" id="pictureOfDay">Home</a>
             <a class="navbar-item" id="recipes">Recipes</a>
@@ -40,12 +39,26 @@ class MenuBarComponent extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         this.shadowRoot.getElementById('pictureOfDay').onclick = (event) => {
             event.preventDefault();
+            this.hideNavbarMenu();
             this.global.routingService.loadPage("picture-of-the-day", true);
         };
         this.shadowRoot.getElementById('recipes').onclick = (event) => {
             event.preventDefault();
+            this.hideNavbarMenu();
             this.global.routingService.loadPage("recipes", true);
         };
+
+        const navbarBurger = this.shadowRoot.querySelector('.navbar-burger');
+        navbarBurger.onclick = (event) => {
+            event.preventDefault();
+            navbarBurger.classList.toggle('is-active');
+            this.shadowRoot.querySelector('.navbar-menu').classList.toggle('is-active');
+        };
+    }
+
+    hideNavbarMenu() {
+        this.shadowRoot.querySelector('.navbar-burger').classList.remove('is-active');
+        this.shadowRoot.querySelector('.navbar-menu').classList.remove('is-active');
     }
 }
 
